42ad5561426d8eff2cb2bb63eae44ffb65ab443c
Chesley Tan <chesleytan97@gmail.com>
2015-02-23 18:22:58 -0500

    Fix 196 algorithm

---
+++ 196-Algorithm-75/196.py
@@ -0,0 +1,25 @@
+'''
+Starting at 1
+bashing out the first: 131027
+bashing out the second: 16814935
+
+Starting at 0
+bashing out the first: 112856
+bashing out the second: ???
+'''
+
+def algo(num):
+    iters = 0
+    while iters < 100:
+        palinstr = str(num)
+        revPalinstr = palinstr[::-1]
+        if palinstr == revPalinstr:
+            return iters
+        num += int(revPalinstr)
+        iters += 1
+
+count = 0
+for i in range(101, 1000000):
+    if algo(i) > 10:
+        count += 1
+print count
