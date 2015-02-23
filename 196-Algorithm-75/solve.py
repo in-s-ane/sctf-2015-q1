662265a4fd50fca6b990a6a2a1ae6606fb974ba9
Danny Qiu <dqiu55@gmail.com>
2015-02-23 16:55:09 -0500

    Add progress for 196-Algorithm

---
+++ 196-Algorithm-75/solve.py
@@ -0,0 +1,34 @@
+'''
+bashing out the first: 113068
+bashing out the second: 14652767
+'''
+
+# recursive solution
+def algo(num, iters):
+    if iters >= 100:
+        return 0
+    rev = int(str(num)[::-1])
+    num += rev
+    palinstr = str(num)
+    if palinstr != palinstr[::-1]:
+        return algo(num, iters+1)
+    else:
+        return iters
+
+# looping solution
+def algo2(num):
+    iters = 1
+    while iters < 100:
+        rev = int(str(num)[::-1])
+        num += rev
+        palinstr = str(num)
+        if palinstr == palinstr[::-1]:
+            return iters
+        iters += 1
+
+#print algo2(5280) # Should return 3
+count = 0
+for i in range(100, 1000000):
+    if algo2(i) > 10:
+        count += 1
+print count
