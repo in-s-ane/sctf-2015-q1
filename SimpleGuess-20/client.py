9b2233928e2ae40a835088d900e3cf817026de50
Danny Qiu <dqiu55@gmail.com>
2015-02-24 00:31:39 -0500

    Fix simple guess since they updated the problem

--- SimpleGuess-20/client.py
+++ SimpleGuess-20/client.py
@@ -1,18 +1,15 @@
 import socket
-import random
 
 HOST = "python.sctf.io"
 PORT = 11234
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-
 while (True):
-    random.seed()
-    secret = random.randint(1 , 11)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    secret = 1
     sock.sendall(str(secret));
     received = sock.recv(1024)
-    if received != "Nope!" or received != "What's your guess?" :
+    if received != "Nope!" and received != "What is your guess?":
         print received
         break
 
