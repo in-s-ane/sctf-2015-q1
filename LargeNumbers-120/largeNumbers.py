5b9c79272e3bb0be62a66fc9c570fb434fab77df
elc1798 <subliminalmau5@gmail.com>
2015-02-24 08:36:35 -0500

    How you do dis

--- LargeNumbers-120/largeNumbers.py
+++ LargeNumbers-120/largeNumbers.py
@@ -9,34 +9,50 @@ n = ""
 p = ""
 
 def a(b):
-	return not [x for x in xrange(2,int(math.sqrt(b))) if b%x == 0]
+    return not [x for x in xrange(2,int(math.sqrt(b))) if b%x == 0]
 	
 def c(d):
-	e = xrange(2,d)
-	f = 2
-	while not y and f in e:
-		if d%f==0 and a(f):
-			y = y + [f] + c(d/f)
-		f += 1
+    e = xrange(2,d)
+    f = 2
+    while not y and f in e:
+        if d%f==0 and a(f):
+            y = y + [f] + c(d/f)
+        f += 1
 
-if len(y) == 0:
-	c(x)
+def encrypt():
+    if len(y) == 0:
+        c(x)
 
-for g in y:
-	z += g
-	z = math.sqrt(z)
-	z *= 1.1
-	z -= (g*0.1)
-	z = abs(z)
+    for g in y:
+        z += g
+        z = math.sqrt(z)
+        z *= 1.1
+        z -= (g*0.1)
+        z = abs(z)
 	
-n = ''.join(str(ord(q)) for q in m)
+    n = ''.join(str(ord(q)) for q in m)
 
-for h in n:
-	if int(h) == 0:
-		p = p + repr(int(z/9999))
-	else:
-		p = p + repr(int(z/int(h)*999))
+    for h in n:
+        if int(h) == 0:
+            p = p + repr(int(z/9999))
+        else:
+            p = p + repr(int(z/int(h)*999))
 
-p = p.replace("L","_")
-p = p[:-1]
-print(p)
+    p = p.replace("L","_")
+    p = p[:-1]
+    print(p)
+
+def decrypt(p):
+    y = []
+    c(x)
+    p = p[:1]
+    p = p.replace("_" ,"L")
+    for g in y:
+        z += g
+        z = math.sqrt(z)
+        z *= 1.1
+        z -= (g * 0.1)
+        z = abs(z)
+    while len(p) > 0:
+        index = p.find("L")
+        a = float(p[0:index + 1])
