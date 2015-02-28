2e9bc0fd7e89dcc45e864027b8c43897f101e9a7
Danny Qiu <dqiu55@gmail.com>
2015-02-28 15:54:20 -0500

    Update Bob's Server crack

--- Bobs-Private-Server-110/crack.py
+++ Bobs-Private-Server-110/crack.py
@@ -2,6 +2,11 @@ from hashlib import sha1
 
 rainbow = ["red", "green", "yellow", "blue", "orange", "indigo", "violet"]
 
+number_color = []
+for num in range(1000, 10000):
+    for color in rainbow:
+        number_color.append("%d%s" % (num, color))
+
 f = open('bobs_info.txt', 'r')
 name = f.readline().strip()
 hashed = f.readline().strip()
@@ -11,11 +16,10 @@ f = open('cat_names.txt', 'r')
 
 for name in f:
     name = name.strip().lower()
-    for num in range(0, 10000):
-        for color in rainbow:
-            password = "%s%04d%s" % (name, num, color)
-            #print password
-            if sha1(password).hexdigest() == hashed:
-                print "%s's password: %s" % (name, password)
-                break
+    for e in number_color:
+        password = name + e
+        #print password
+        if sha1(password).hexdigest() == hashed:
+            print "%s's password: %s" % (name, password)
+            break
     print name
