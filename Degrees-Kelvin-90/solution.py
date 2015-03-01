cc020d1ce831de17a3396c25481507d672166e31
Danny Qiu <dqiu55@gmail.com>
2015-03-01 00:49:28 -0500

    Add cringe-worthy bruteforce progress to degrees kelvin

--- Degrees-Kelvin-90/solution.py
+++ Degrees-Kelvin-90/solution.py
@@ -1,29 +1,21 @@
-"""This is so not gonna work, simple greedy algorithm"""
-
-import sys
-
-score = 0 # score counter
+import sys, copy
 
 f = open(sys.argv[1], "r").read().split("\n")
-
 f = filter(None, f)
+#print f
 
-print f
-
+SCORE = 0 # score counter
 problemList = []
-worthList = []
 
 for i in range(1, len(f)):
     data = f[i].split(" ")
     problemWorth = int(data[0])
     deduction = int(data[1])
     timeToSolve = int(data[2])
+    # Only put problems that are still worth points after Kelvin solves it
     if (problemWorth - (deduction * timeToSolve)) > 0:
         realWorth = problemWorth - (deduction * timeToSolve)
-    else:
-        realWorth = 0
-    problemList.append([deduction, timeToSolve])
-    worthList.append(realWorth)
+        problemList.append([realWorth, deduction, timeToSolve])
 
 def maxIndex(list1): # linear search that returns index
     currentMax = list1[0]
@@ -31,19 +23,39 @@ def maxIndex(list1): # linear search that returns index
     for i in range(1, len(list1)):
         if list1[i] > currentMax:
             index = i
-    return index
+            currentMax = list1[i]
+    return index if currentMax != 0 else -1
 
-# GREEDY ALGORITHM!! <-- not gonna work
+# HIGHLY INEFFICIENT BRUTE FORCE. CRINGE. WORTHY. O(N!) is NOT a good runtime.
 
-while worthList != []:
-    problemNumber = maxIndex(worthList)
+def isZero(input_list):
+    for i in input_list:
+        if i != 0:
+            return False
+    return True
 
-    score += worthList.pop(problemNumber)
-    for i in range(0, len(worthList)):
-        if (worthList[i] - (problemList[problemNumber][1] * problemList[i][0])) > 0:
-            worthList[i] -= problemList[problemNumber][1] * problemList[i][0]
-        else:
-            worthList[i] = 0
-    problemList.pop(problemNumber)
+def solve(problems):
+    if not isZero(problems):
+        for i in range(0, len(problems)):
+            problems_copy = copy.deepcopy(problems)
+            problem = problems_copy.pop(i)
+            for j in range(0, len(problems_copy)):
+                problems_copy[j][0] -= (problem[2] * problems_copy[j][1])
+                if problems_copy[j][0] < 0:
+                    problems_copy[j][0] = 0
+            return problem[0] + solve(problems_copy)
+    return 0
 
-print score
+for i in range(0, len(problemList)):
+    problems_copy = copy.deepcopy(problemList)
+    problem = problems_copy.pop(i)
+    for j in range(0, len(problems_copy)):
+        problems_copy[j][0] -= problem[2] * problems_copy[j][1]
+        if problems_copy[j][0] < 0:
+            problems_copy[j][0] = 0
+    total = problem[0] + solve(problems_copy)
+    if total > SCORE:
+        SCORE = total
+    print i
+
+print SCORE
